eb47894ad1c4ce0c17fa0928d40d96836ec62f5d
Coder-MinYi <2376108021@qq.com>
2020-11-23 23:55:09 +0800

    寻找第K大（快排思想）

---
+++ 1123/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created with InteIIiJ IDEA.
+ * Description:
+ * User:MINYI
+ * Date:
+ * Time:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.round(11.5));
+        System.out.println(Math.round(-11.5));
+    }
+}
---
+++ 1123/src/Myclass.java
@@ -0,0 +1,14 @@
+/**
+ * Created with InteIIiJ IDEA.
+ * Description:
+ * User:MINYI
+ * Date:
+ * Time:
+ */
+public class Myclass {
+    static int i;
+
+    public static void main(String[] args) {
+        System.out.println(i);
+    }
+}
